a219ebee54b910dddb19f03166f64173dbf27a6a
Claudia Orellana <cgorellana2007@gmail.com>
2026-09-21 18:29:51 -0600

    Trabajo clase martes semana 2

---
+++ variable02.py
@@ -0,0 +1,55 @@
+# Vamos a crear una variable
+monto = 123.45
+alumno = "Javier"
+edad = 21
+aprobado = True
+"""
+variable = 1
+print(type(variable))
+variable = 12.56
+print(type(variable))
+variable = "dos"
+print(type(variable))
+variable = False
+print(type(variable))
+"""
+
+
+"""deudor = "Alvin"
+monto_deuda = 500
+pendiente_deuda = False
+saldo_actual = monto_deuda * pendiente_deuda
+print(saldo_actual)"""
+
+"""compras_semana = 789
+cantidad_compras = 0
+
+compra_lunes = 100
+compras_semana += compra_lunes
+cantidad_compras += 1
+
+compra_martes = 200
+compras_semana += compra_martes
+cantidad_compras += 1
+
+compra_miércoles = 300
+compras_semana += compra_miércoles
+cantidad_compras += 1
+
+compra_jueves = 400
+compras_semana += compra_jueves
+cantidad_compras += 1
+
+compra_viernes = 500
+compras_semana += compra_viernes
+cantidad_compras += 1 """
+
+comprador = "Jony"
+vendedor = "Carlitos"
+compra = 25.75
+compra_con_impuestos = compra * 2
+# print(compra_con_impuestos)
+# print(comprador + "le compró a" + vendedor)
+print(f"{comprador} le compró a {vendedor} {round(compra * 1.10, 2)}")
+print(round(12.3456789))
+round()
